35eebb9d6c2da2ebe5dff97f424da55ff0f49f30
Kevin Zhu <kevinzhu@Kevins-Air.cpe.cableonda.net>
2022-07-08 15:37:40 -0500

    Create an array of objects of dimsum dishes

---
+++ src/Components/cardInfo.js
@@ -0,0 +1,104 @@
+import stickyRice from './dim-sum-dishes/sticky-rice-lotus-leaf.jpeg'
+import eggTarts from './dim-sum-dishes/egg-tarts.jpeg'
+import potStickers from './dim-sum-dishes/potstickers.jpeg'
+import steamedPorkRibs from './dim-sum-dishes/steamed-pork-ribs.jpeg'
+import soupDumplings from './dim-sum-dishes/soup-dumplings.jpeg'
+import beefTripe from './dim-sum-dishes/beef-tripe.jpeg'
+import porkRiceNoodles from './dim-sum-dishes/pork-rice-noodles.jpeg'
+import scallionPancakes from './dim-sum-dishes/scallion-pancakes.jpeg'
+import bakedPorkBuns from './dim-sum-dishes/baked-pork-buns.jpeg'
+import plainRiceNoodles from './dim-sum-dishes/plain-rice-noodles.jpeg'
+import turnipCakes from './dim-sum-dishes/turnip-cakes.jpeg'
+import friedSesameBalls from './dim-sum-dishes/fried-sesame-balls.jpeg'
+import shrimpDumplings from './dim-sum-dishes/shrimp-dumplings.jpg'
+import porkDumplings from './dim-sum-dishes/pork-dumplings.png'
+import chickenFeet from './dim-sum-dishes/chicken-feet.jpeg'
+import spongeCake from './dim-sum-dishes/sponge-cake.jpeg'
+import friedSpringRolls from './dim-sum-dishes/fried-spring-rolls.jpeg'
+import steamedPorkBuns from './dim-sum-dishes/steamed-pork-buns.jpeg'
+import sesamePrawnToast from './dim-sum-dishes/sesame-prawn-toasts.jpeg'
+import pineappleCustardBuns from './dim-sum-dishes/pineapple-custard-buns.jpeg'
+
+export const dimsumDishes = [
+    {
+        "name": "Sticky Rice",
+        "source": stickyRice,
+    },
+    {
+        "name": "Egg Tarts",
+        "source": eggTarts,
+    },
+    {
+        "name": "Potstickers",
+        "source": potStickers
+    },
+    {
+        "name": "Steamed Pork Ribs",
+        "source": steamedPorkRibs
+    },
+    {
+        "name": "Soup Dumplings",
+        "source": soupDumplings
+    },
+    {
+        "name": "Beef Tripe",
+        "source": beefTripe
+    },
+    {
+        "name": "Pork Rice Noodles",
+        "source": porkRiceNoodles
+    },
+    {
+        "name": "Scallion Pancakes",
+        "source": scallionPancakes
+    },
+    {
+        "name": "Baked Pork Buns",
+        "source": bakedPorkBuns
+    },
+    {
+        "name": "Plain Rice Noodles",
+        "source": plainRiceNoodles
+    },
+    {
+        "name": "Turnip Cakes",
+        "source": turnipCakes
+    },
+    {
+        "name": "Fried Sesame Balls",
+        "source": friedSesameBalls
+    },
+    {
+        "name": "Shrimp Dumplings",
+        "source": shrimpDumplings
+    },
+    {
+        "name": "Pork Dumplings",
+        "source": porkDumplings
+    },
+    {
+        "name": "Chicken Feet",
+        "source": chickenFeet
+    },
+    {
+        "name": "Sponge Cake",
+        "source": spongeCake
+    },
+    {
+        "name": "Fried Spring Rolls",
+        "source": friedSpringRolls
+    },
+    {
+        "name": "Steamed Pork Buns",
+        "source": steamedPorkBuns
+    },
+    {
+        "name": "Sesame Prawn Toast",
+        "source": sesamePrawnToast
+    },
+    {
+        "name": "Pineapple Buns",
+        "source": pineappleCustardBuns
+    },
+]
+
