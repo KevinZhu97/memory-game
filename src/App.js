6e45e59076d8d1a272ad3d00aebeb6e5a6c18048
Kevin Zhu <kevinzhu@Kevins-Air.cpe.cableonda.net>
2022-07-09 18:24:56 -0500

    Address bad setState() call in Main.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Header from './Components/header'
 import Main from './Components/main'
 import Footer from './Components/footer'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function App() {
 
   const [currentScore, setCurrentScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
 
+  
   const getScores = (currentScore, highScore) => {
     setCurrentScore(currentScore)
     setHighScore(highScore)
--- src/Components/main.js
+++ src/Components/main.js
@@ -1,40 +1,38 @@
 import CardGrid from './cardGrid'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Main = ({getScores}) => {
     const [currentScore, setCurrentScore] = useState(0);
     const [highScore, setHighScore] = useState(0);
     const [cardsClicked, setCardsClicked] = useState([]);
 
+    const getDish = (e) => {
+            const dishName = e.target.parentNode.lastChild.textContent;
+            playRound(dishName)
+    }
+
+    const playRound = (dishName) => {
+            if (cardsClicked.includes(dishName)) {
+                resetGame()
+            } else {
+                let newScore = currentScore + 1 
+                if (newScore > highScore) {
+                    setHighScore(newScore)
+                }
+                setCurrentScore(newScore)
+                setCardsClicked((prevState) => [...prevState, dishName])
+                console.log(newScore)
+                console.log(cardsClicked)
+            };
+    } 
+
     const resetGame = () => {
         setCardsClicked([])
         setCurrentScore(0)
     }
 
-    const getDish = (e) => {
-        const dishName = e.target.parentNode.lastChild.textContent;
-        playRound(dishName)
-    
-    }
-
-    const playRound = (dishName) => {
-        if (cardsClicked.includes(dishName)) {
-            resetGame()
-        } else {
-            let newScore = currentScore + 1 
-            if (newScore > highScore) {
-                setHighScore(newScore)
-            }
-            setCurrentScore(newScore)
-            setCardsClicked((prevState) => [...prevState, dishName])
-            console.log(newScore)
-            console.log(cardsClicked)
-
-        }
-    }
-
     return(
-        <div style={container} sendScores={getScores(currentScore, highScore)}>
+        <div style={container} sendscores={getScores(currentScore, highScore)}>
             <CardGrid handleCardClick={getDish}/>
         </div>
     )
